f486ff99a0dec555a15b6d780f99b6ea4f71bfce
happytech <happytech.web@gmail.com>
2026-01-14 20:37:06 +0800

    [script] add dbg script

---
+++ build_skill_id_dbg.sh
@@ -0,0 +1,3 @@
+python scripts/build_calvin_task_index.py /home/humanoid/linglong/vpp/video-prediction-policy/calvin/dataset/calvin_debug_dataset \
+    --lang-folder lang_annotations \
+    --out ./calvin_task_index_debug.json
